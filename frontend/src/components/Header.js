a8edd4549dd73544d698de5c9dae8c62856d3a0c
Chris Roan <christophertroan@gmail.com>
2023-10-07 10:04:01 -0400

    fixed code with linter

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-  backgroundColor: 'lightblue'  //camelCase notation is important
+  backgroundColor: 'lightblue', //camelCase notation is important
 };
 
-const Header = ( { title } ) => {
-    return (
-        <Navbar style={navbarStyle} data-bs-theme="light">
-          <Container>
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-          </Container>
-        </Navbar>
-    )
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 // const Header = ( props ) => {             #this is another way to write the above Header component/properties for title
