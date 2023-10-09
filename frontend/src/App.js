88f80a327f186645bcfd16c857da6793cdc0f7c7
Chris Roan <christophertroan@gmail.com>
2023-10-09 08:21:55 -0400

    save images in the state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY; // this variable will b
 
 const App = () => {
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]); //there are no images when the app starts, so there should be nothing in this array (images array), yet.
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -18,8 +21,7 @@ const App = () => {
     )
       .then((res) => res.json()) //callback function that is executed when promise is resolved. here we call json method to res. res is passed to the callback function as an argument. it will also return the promsie, so we need to resolve it again
       .then((data) => {
-        //this is where we get access to data object that is sent from server to client
-        console.log(data);
+        setImages([data, ...images]); //we put data in the beginning of the array because we want to the new data (image) to appear first. this will update array everytime an image is searched
       })
       .catch((err) => {
         //catch will be called in case promise is rejected (if there is an error during fetch request)
@@ -27,9 +29,9 @@ const App = () => {
       });
     setWord('');
   };
-  // (e.target[0].value) shows value of input of target (inspect > console > click the event > target)
+  //(e.target[0].value) shows value of input of target (inspect > console > click the event > target)
   //console.log(word); gives us an updated state every time a key is pressed, not when it is searched - controlled state
-  // console.log(process.env.REACT_APP_UNSPLASH_KEY); // this will show us process.env object in console of website. It will show us specifially our unplash key
+  //console.log(process.env.REACT_APP_UNSPLASH_KEY); // this will show us process.env object in console of website. It will show us specifially our unplash key
   return (
     <div className="App">
       <Header title="Images Gallery" />
