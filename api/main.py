5cf008cb041cb9cf902afea23f1d9ac511ece6bd
Chris Roan <christophertroan@gmail.com>
2023-11-16 09:02:27 -0500

    added request to the unsplash api

--- api/main.py
+++ api/main.py
@@ -1,12 +1,31 @@
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = (os.environ.get("UNSPLASH_KEY", ""))
+
+if not UNSPLASH_KEY:
+   raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY there")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-   return "<p>Hello, World!</p>"
+
+@app.route("/new-image") # only GET method will be allowed 
+def new_image():
+   word = request.args.get("query")
+   headers = {
+      "Accept-Version": "v1", 
+      "Authorization": "Client-ID " + UNSPLASH_KEY # string concatenation
+   }
+   params = {"query": word}
+   response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+
+   data = response.json()
+   return data
 
 if __name__ == "__main__":        # This makes it to where this app will only run if it is ran directly
-   app.run(host="0.0.0.0", port=5050) 
-   # Flask will be running on our computer and will be available via any of the IP addresses assigned to this computer, including localhost IP
-   # with Keyword (named) arguments, order of the arguments in the function call does not matter
+   app.run(host="0.0.0.0", port=5050) # Flask will be running on our computer and will be available via any of the IP addresses assigned to this computer, including localhost IP
