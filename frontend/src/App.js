0d4f092403e2101f08237f6b6ac60280d518738c
Chris Roan <christophertroan@gmail.com>
2023-12-08 09:53:18 -0500

    introduce axios and rewrote new-image fetch call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.js';
 import Search from './components/Search.js';
@@ -14,22 +15,18 @@ const App = () => {
   const [word, setWord] = useState(''); //word is the variable, and setWord is the function we use to update the word variable
   const [images, setImages] = useState([]); //there are no images when the app starts, so there should be nothing in this array (images array), yet.
 
-  const handleSearchSubmit = (e) => {
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    fetch(`${API_URL}/new-image?query=${word}`)
-      .then((res) => res.json()) //callback function that is executed when promise is resolved. here we call json method to res. res is passed to the callback function as an argument. it will also return the promsie, so we need to resolve it again
-      .then((data) => {
-        setImages([{ ...data, title: word }, ...images]); //we put data in the beginning of the array because we want to the new data (image) to appear first. this will update array everytime an image is searched
-      })
-      .catch((err) => {
-        //catch will be called in case promise is rejected (if there is an error during fetch request)
-        console.log(err);
-      });
+
+    try {
+      const res = await axios.get(`${API_URL}/new-image?query=${word}`);
+      setImages([{ ...res.data, title: word }, ...images]);
+    } catch (error) {
+      console.log(error);
+    }
+
     setWord('');
   };
-  //(e.target[0].value) shows value of input of target (inspect > console > click the event > target)
-  //console.log(word); gives us an updated state every time a key is pressed, not when it is searched - controlled state
-  //console.log(process.env.REACT_APP_UNSPLASH_KEY); // this will show us process.env object in console of website. It will show us specifially our unplash key
 
   const handleDeleteImage = (id) => {
     //id is the id of each image from unsplash API, can be found in web pageconsole>components>State arrays
